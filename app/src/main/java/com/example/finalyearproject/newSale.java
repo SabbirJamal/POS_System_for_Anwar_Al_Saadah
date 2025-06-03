1770ac5a45f29a858c9726f09470ab396eea7c1d
shker <21s21397@mec.edu.om>
2025-06-04 00:44:30 +0400

    sales  Commit

--- app/src/main/java/com/example/finalyearproject/newSale.java
+++ app/src/main/java/com/example/finalyearproject/newSale.java
@@ -2,6 +2,7 @@ package com.example.finalyearproject;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -26,8 +27,18 @@ public class newSale extends AppCompatActivity {
     //total amount fields
     TextView dt,mt,kt,at,st,munt;
 
-    //initializing quantity;
-    double cost,ctd,ctm,ctk,cta,cts,ctmun;
+    //per item price textfield
+    TextView dp,mp,kp,ap,sp,munp;
+
+    //for storing items names
+    String i1,i2,i3,i4,i5,i6;
+
+    //button for calculation
+    Button c;
+
+    //finding the calc java class
+    private salecalculation sc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,5 +77,118 @@ public class newSale extends AppCompatActivity {
         at=findViewById(R.id.anabitotamt);
         st=findViewById(R.id.shailatotamt);
         munt=findViewById(R.id.mundeeltotamt);
+
+        //identifying per item price textfields
+        dp=findViewById(R.id.dressprice);
+        mp=findViewById(R.id.moriollaprice);
+        kp=findViewById(R.id.khameezprice);
+        ap=findViewById(R.id.anabiprice);
+        sp=findViewById(R.id.shailaprice);
+        munp=findViewById(R.id.mundeelprice);
+
+        sc=new salecalculation();
+
+        cbd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dp.setText("8");
+                i1="Dress";
+            }
+        });
+
+        cbm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp.setText("5");
+                i2="Moriolla";
+            }
+        });
+
+        cbk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                kp.setText("4");
+                i3="Khameez";
+            }
+        });
+
+        cba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ap.setText("4");
+                i4="Anabi";
+            }
+        });
+
+        cbs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sp.setText("3");
+                i5="Shaila";
+            }
+        });
+
+        cbmun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                munp.setText("2");
+                i6="Mundeel";
+            }
+        });
+
+        c=findViewById(R.id.calc);
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //getting the price per item amt
+                String damt=dp.getText().toString();
+                String mamt=mp.getText().toString();
+                String kamt=kp.getText().toString();
+                String aamt=ap.getText().toString();
+                String samt=sp.getText().toString();
+                String munamt=munp.getText().toString();
+
+                //getting quantity per item
+                String dquan=dq.getText().toString();
+                String mquan=mq.getText().toString();
+                String kquan=kq.getText().toString();
+                String aquan=aq.getText().toString();
+                String squan=sq.getText().toString();
+                String munquan=munq.getText().toString();
+
+
+                //converting price string to double
+                Double ddamt=Double.parseDouble(damt);
+                Double dmamt=Double.parseDouble(mamt);
+                Double dkamt=Double.parseDouble(kamt);
+                Double daamt=Double.parseDouble(aamt);
+                Double dsamt=Double.parseDouble(samt);
+                Double dmunamt=Double.parseDouble(munamt);
+
+
+                //converting quantity string to double
+                Double ddquan=Double.parseDouble(dquan);
+                Double dmquan=Double.parseDouble(mquan);
+                Double dkquan=Double.parseDouble(kquan);
+                Double daquan=Double.parseDouble(aquan);
+                Double dsquan=Double.parseDouble(squan);
+                Double dmunquan=Double.parseDouble(munquan);
+
+                Double c1=sc.salecalc(ddamt,ddquan);
+                Double c2=sc.salecalc(dmamt,dmquan);
+                Double c3=sc.salecalc(dkamt,dkquan);
+                Double c4=sc.salecalc(daamt,daquan);
+                Double c5=sc.salecalc(dsamt,dsquan);
+                Double c6=sc.salecalc(dmunamt,dmunquan);
+
+                dt.setText(""+c1);
+                mt.setText(""+c2);
+                kt.setText(""+c3);
+                at.setText(""+c4);
+                st.setText(""+c5);
+                munt.setText(""+c6);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/finalyearproject/salecalculation.java
@@ -0,0 +1,10 @@
+package com.example.finalyearproject;
+
+public class salecalculation {
+    private double amt,quan;
+    public static double salecalc(double amt, double quan)
+    {
+        return(amt*quan);
+    }
+
+}
